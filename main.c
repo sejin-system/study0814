1b04e834e665548ee52ce60ec6ebc4663f5acc92
sejin <sejin.system.kr@gmail.com>
2020-08-14 14:07:04 +0900

    add main.c

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main()
-{
+{	int n, s;
+
+	printf("n = ?\n");
+
+	scanf("%d", &n);
+	
+	if(n>0){
+		s = n*(n+1)/2;
+		printf("%d\n", s);
+	}
+
+	else printf("프로그램 종료\n");
+
 	return 0;
 }
 
